ad311cd28833b4febf3efccdcc60e8fe83e242f4
Andrei Dobre <75906525+andreiddobre@users.noreply.github.com>
2023-04-24 22:39:01 +0300

    Add files via upload

---
+++ Numpy/Example.02.Numpy.Arange() Along With Einsum().py
@@ -0,0 +1,16 @@
+#importing required modules
+import numpy as np
+
+#creating the numpy arrays
+arr1=np.arange(18,36,2).reshape(3,3)
+arr2=np.arange(38,56,2).reshape(3,3)
+
+#displaying the original arrays
+print("The first array or matrix is =",arr1)
+print("The second array or matrix is =",arr2)
+
+#calculating the einstein summation
+result = np.einsum("ij,jk", arr1, arr2)
+
+#displaying the einstein summation of the given arrays
+print("The Einstein Summation is =", result)
